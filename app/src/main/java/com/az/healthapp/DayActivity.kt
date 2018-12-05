786c2e5a63d36eb4bf91daa491a82172c31d42e5
aleksandrazubarchuk <az@gen.tech>
2018-12-05 19:01:40 +0200

    change day activity

---
+++ app/src/main/java/com/az/healthapp/DayActivity.kt
@@ -0,0 +1,12 @@
+package com.az.healthapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DayActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_day)
+    }
+}
