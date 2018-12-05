3a4e8318c81ea290f4813f7c2b7d5f91a20e3cfc
taras prots <taras@duomo.world>
2018-12-05 18:53:14 +0200

    Sudoku activity

---
+++ app/src/main/java/com/az/healthapp/SudokuActivity.kt
@@ -0,0 +1,12 @@
+package com.az.healthapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SudokuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sudoku)
+    }
+}
